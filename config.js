5e9183b4267be0bddbaf300a5913a47b3302107e
tenquius <87209189+tenquius@users.noreply.github.com>
2021-07-09 18:06:16 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,28 @@
+global.owner = ['5491127640079'] // Put your number here
+global.mods = ['5491127640079'] // Want some help?
+global.prems = ['5491127640079','5493412651417','5491160421156','5492645068890','5491125200816','642776797777'] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'd90a9e986e18778b'
+}
+
+// Sticker WM
+global.packname = ' Insane BOT '
+global.author = 'segui a @insanebot.oficial '
+
+global.multiplier = 69 // The higher, The harder levelup
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
