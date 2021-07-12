7019932d4d5ec76e5b831348acf9f397017fbeeb
tenquius <87209189+tenquius@users.noreply.github.com>
2021-07-12 16:55:37 -0300

    Update trabajo.js

--- plugins/trabajo.js
+++ plugins/trabajo.js
@@ -1,5 +1,5 @@
 let handler = async m => m.reply(`
-▫️👉[empleo 1]
+▫️👉[empleo 1] 
 LABURA desde CASA
 Regístrate y gana de 100 a 300 pesos
 reales mirando vídeos. 
@@ -7,30 +7,13 @@ reales mirando vídeos.
 hasta 8 mil pesos semanale$$
 
 ▫️Registrate con el codigo
-https://s.kwai.app/s/odEMde9C
+https://vm.tiktok.com/ZMdVffAGu/
 
 *Luego cuando finalizes de registrarte*
 *pone .verificar*
 *para ganar premium en InsaneBOT*
 _______________________________
 
-▫️👉[empleo 2]
-LABURA desde CASA
-Regístrate también acá
-y gana de 100 a 300 pesos
-reales mirando vídeos. 
-▫️OPORTUNIDAD de ganar 
-hasta 8 mil pesos semanale$$
-▫️Registrate con el codigo
-https://vm.tiktok.com/ZMdaXXdn3/
-codigo.L754678251
-
-*Luego cuando finalizes de registrarte*
-*pone .verificar*
-*para ganar premium en InsaneBOT*
-
-_*Si tienes los dos empleos 
-también ganaras 60.000 exp*_
                                         
 `.trim()) 
 handler.help = ['trabajo','premium']
