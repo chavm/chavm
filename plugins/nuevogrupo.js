31c427a0597beaae92c6718cde0068c2b1a8bc31
tenquius <87209189+tenquius@users.noreply.github.com>
2021-07-20 00:30:35 -0300

    Update nuevogrupo.js

--- plugins/nuevogrupo.js
+++ plugins/nuevogrupo.js
@@ -5,7 +5,7 @@ let handler  = async (m, { conn, text}) => {
     conn.reply(global.owner + '@s.whatsapp.net',`*Grupo enviado de: @${m.sender.split('@')[0]}*\n\n${text}`, m,{contextInfo: {
       mentionedJid: [m.sender]
     }})
-    conn.reply(m.chat,`Se envió el grupo a revisión\n\nSegui a @insanebot.oficial en ig\n\nSabías el hecho importante de que los grupos tienen que tener al menos mas de 25 participantes`,m)
+    conn.reply(m.chat,`Se envió el grupo a revisión\nMomentaneamente el bot esta lleno de grupos(214 grupos)\nSe avisará por instagram si abrimos mas números con el bot dentro de una semana \nSegui a @insanebot.oficial en ig\n\nSabías el hecho importante de que los grupos tienen que tener al menos mas de 25 participantes`,m)
   }
 }
 handler.help = ['nuevogrupo *link*']
