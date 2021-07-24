1ab6e72de2eb9bdb41154fe8c8f8e95eff11f3dc
tenquius <87209189+tenquius@users.noreply.github.com>
2021-07-24 18:56:09 -0300

    Update profile.js

--- plugins/profile.js
+++ plugins/profile.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn }) => {
   try {
     pp = await conn.getProfilePicture(who)
   } catch (e) {
-          return;
+          handler
   } finally {
     let about = (await conn.getStatus(who).catch(console.error) || {}).status || ''
     let { name, limit, exp, lastclaim, registered, warning, robos, regTime, age, level } = global.DATABASE.data.users[who]
