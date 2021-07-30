9772b968b27a953c007b25e9f5caac5740cfb7a5
tenquius <87209189+tenquius@users.noreply.github.com>
2021-07-29 23:10:10 -0300

    Update vote-start.js

--- plugins/vote-start.js
+++ plugins/vote-start.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, usedPrefix }) => {
     conn.vote = conn.vote ? conn.vote : {}
     let id = m.chat
     if (id in conn.vote) {
-        return conn.reply(m.chat, `TODAVIA HAY VOTACIONES! para eliminar pone .borrarvotacion`, m)
+        return conn.reply(m.chat, `TODAVIA HAY VOTACIONES!\n para eliminar pone .borrarvotacion \n para ver las votaciones .checkear`, m)
     }
     m.reply(`¡La votación comienza!\n*${usedPrefix}si* - para si\n*${usedPrefix}no* - para no\n*${usedPrefix}chekear* - para chekear el voto\n*${usedPrefix}borrarvotacion* - para eliminar`)
     conn.vote[id] = [
