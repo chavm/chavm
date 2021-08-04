db885af2013aadd505d744ae3f3bfc463f187066
tenquius <87209189+tenquius@users.noreply.github.com>
2021-08-04 01:58:04 -0300

    Update trabajo.js

--- plugins/trabajo.js
+++ plugins/trabajo.js
@@ -11,22 +11,7 @@ https://s.kwai.app/s/odEMde9C
 *pone .verificar*
 *para ganar premium en InsaneBOT*
 _______________________________
-▫️👉[empleo 2]
-LABURA desde CASA
-Regístrate también acá
-y gana de 100 a 300 pesos
-reales mirando vídeos. 
-▫️OPORTUNIDAD de ganar 
-hasta 8 mil pesos semanale$$
-▫️Registrate con el codigo
-https://vm.tiktok.com/ZMdaXXdn3/
-codigo.L754678251
-*Luego cuando finalizes de registrarte*
-*pone .verificar*
-*para ganar premium en InsaneBOT*
-_*Si tienes los dos empleos 
-también ganaras 60.000 exp*_
-                                        
+
 `.trim()) 
 handler.help = ['trabajo','premium']
 handler.tags = ['xp']
