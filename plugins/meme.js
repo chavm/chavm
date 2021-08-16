89a635e0b29e8f1b6529927be8f4e938da2aef50
tenquius <87209189+tenquius@users.noreply.github.com>
2021-08-16 15:26:29 -0300

    Update meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -1,25 +1,17 @@
 let fetch = require('node-fetch')
 //plugin by Samu330
-try {
-let handler  = async (m, { conn }) => {
+
+let handler  = async (m, { conn, text }) => {
+if (!text) return conn.reply(m.chat, 'No hay texto para buscar imagen', m)
 let url = await fetch('https://insanebott.000webhostapp.com/meme')
 //let url = await fetch('https://api.fdci.se/sosmed/rep.php?gambar=' + encodeURIComponent(text))
-
 let samu = await url.json()
 let erest = samu[Math.floor(Math.random() * samu.length)]
-            await conn.sendFile(m.chat, erest, '', 'meme', m)
-            if (!e) console.log(e)
-            
- }
+            await conn.sendFile(m.chat, erest, '', 'FUERON DEMACIADAS IMAGENES DE *' + encodeURIComponent(text) + '* ESPERO TE GUSTE ESTA!!🥰', m)
 }
-else(e) {
-         console.log(e)
-
-}
-     
-handler.help = ['meme']
+handler.help = ['imagen']
 handler.tags = ['images']
-handler.command = /^meme?$/i
+handler.command = /^imagen?$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
