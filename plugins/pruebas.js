c0a75f240f3eaa792181c6ad70cc1f0155fe7c40
tenquius <87209189+tenquius@users.noreply.github.com>
2021-08-20 22:33:42 -0300

    Update pruebas.js

--- plugins/pruebas.js
+++ plugins/pruebas.js
@@ -1,5 +1,6 @@
 let { MessageType } = require('@adiwajshing/baileys')
 let pajak = 0.02
+let handler = async (m, { conn, text }) => {
   if (!text) return conn.reply(m.chat, `es .pagar @mension cantidad`, m)
   let who
   if (m.isGroup) who = m.mentionedJid[0]
@@ -13,7 +14,7 @@ let pajak = 0.02
   exp += pjk
   if (exp < 26) return conn.reply(m.chat, `minimo 26`, m)
   let users = global.DATABASE._data.users
-  users[who].exp -= exp
+  users[m.sender].exp -= exp
   
   m.reply(`Más impuestos gastaste  *${-exp} XP*`)
   conn.fakeReply(m.chat, `te enviaron *+${xp} XP*`, who, m.text)
