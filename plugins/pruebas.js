b6db60992f63cf1a5b217e0e00204f1c1740081f
tenquius <87209189+tenquius@users.noreply.github.com>
2021-08-20 22:08:22 -0300

    Update pruebas.js

--- plugins/pruebas.js
+++ plugins/pruebas.js
@@ -2,9 +2,10 @@ let { MessageType } = require('@adiwajshing/baileys')
 let pajak = 0.02
 let handler = async (m, { conn, text }) => {
   if (!text) return conn.reply(m.chat, `es .pagar @mension cantidad`, m)
-  let who 
-  if (m.isGroup) who = m.mentionedJid[0]
-  let txt = text.replace(text.split`@`[0], '').trim()
+  let who = text
+    if (m.isGroup) who = m.mentionedJid[0]
+    else who = m.chat
+  let txt = text.replace('@' + who.split`@`[0], '').trim()
   let xp = parseInt(txt)
   if (isNaN(xp)) return conn.reply(m.chat, `es .pagar @mension cantidad`, m)
   let exp = xp
@@ -12,16 +13,17 @@ let handler = async (m, { conn, text }) => {
   exp += pjk
   if (exp < 26) return conn.reply(m.chat, `minimo 26`, m)
   let users = global.DATABASE._data.users
+  if (exp > users[m.sender].exp) return conn.reply(m.chat, `no tenes esa exp`, m)
   users[who].exp += xp
   
   m.reply(`Más impuestos gastaste  *${-exp} XP*`)
   conn.fakeReply(m.chat, `te enviaron *+${xp} XP*`, who, m.text)
 }
-handler.help = ['donar @user <amount>']
-handler.tags = ['owner']
-handler.command = /^donar$/
-handler.rowner = true
-handler.group = false
+handler.help = ['pagar @user <amount>']
+handler.tags = ['xp']
+handler.command = /^pagar$/
+handler.rowner = false
+handler.group = true
 handler.limit = true
 
-module.exports = handler
+module.exports = handlerr
