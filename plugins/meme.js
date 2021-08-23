ae4a3f09dfa470c7707ddf6fde4838e0c0dc1388
chavm <89366314+chavm@users.noreply.github.com>
2021-08-22 23:20:55 -0300

    Add files via upload

---
+++ plugins/meme.js
@@ -0,0 +1,25 @@
+let fetch = require('node-fetch')
+//plugin by Samu330
+
+let handler  = async (m, { conn }) => {
+let url = await fetch('https://insanebott.000webhostapp.com/meme')
+//let url = await fetch('https://api.fdci.se/sosmed/rep.php?gambar=' + encodeURIComponent(text))
+let samu = await url.json()
+let erest = samu[Math.floor(Math.random() * samu.length)]
+            await conn.sendFile(m.chat, erest, '', 'MEME', m)
+}
+handler.help = ['meme','memes']
+handler.tags = ['images']
+handler.command = /^(meme|memes)?$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
