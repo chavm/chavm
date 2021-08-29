179586e60713dbc19da2f4cc310822196349526d
chavm <89366314+chavm@users.noreply.github.com>
2021-08-29 16:24:05 -0300

    Update buylimit.js

--- plugins/buylimit.js
+++ plugins/buylimit.js
@@ -1,5 +1,6 @@
 const xpperlimit = 500 
 let handler = async (m, { conn, command, args }) => {
+  if (new Date - global.DATABASE._data.users[m.sender].lastrob > 7000) {
   let count = command.replace(/^buy/i, '')
   count = count ? /all/i.test(count) ? Math.floor(global.DATABASE._data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
   count = Math.max(1, count)
@@ -8,6 +9,7 @@ let handler = async (m, { conn, command, args }) => {
     global.DATABASE._data.users[m.sender].limit += count
     conn.reply(m.chat, `-${xpperlimit * count} XP\n+ ${count} Limit`, m)
   } else conn.reply(m.chat, `XP no es suficiente para comprar ${count} limit`, m)
+} else m.reply('recien robaste..\ntodavia no puedes comprar')
 }
 handler.help = ['buy <limit>', 'buy <limit>', 'buyall']
 handler.tags = ['xp']
@@ -25,4 +27,3 @@ handler.fail = null
 handler.exp = 0
 
 module.exports = handler
-
