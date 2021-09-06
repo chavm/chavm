35bff5f55d91e135befceb5dfaee67a89001bfd1
chavm <89366314+chavm@users.noreply.github.com>
2021-09-06 03:23:36 -0300

    Update carta.js

--- plugins/carta.js
+++ plugins/carta.js
@@ -1,35 +1,20 @@
 let fetch = require('node-fetch')
 //plugin by Samu330
-async function retryResize(options, retries = 0) {
-    let { imagePath, size, quality = 60, maxRetries = 5 } = options;
+let Rimage = require('../lib/image')
+
   
 let handler  = async (m, { conn }) => {
-if (new Date - global.DATABASE._data.users[m.sender].yugi > 2000000) {
+if (new Date - global.DATABASE._data.users[m.sender].yugi > 3000000) {
 let url = await fetch('https://insanebott.000webhostapp.com/yugio')
 //let url = await fetch('https://api.fdci.se/sosmed/rep.php?gambar=' + encodeURIComponent(text))
 let samu = await url.json()
 let erest = samu[Math.floor(Math.random() * samu.length)]
 
             await
-            let image = null;
-    try {
-        image = await Jimp.read(imagePath);
-        await image.resize(size, Jimp.AUTO);
-        await image.quality(quality);
-    } catch (e) {
-        if (retries >= maxRetries) {
-            throw e;
-        }
-                
-        image = await retryResize(options, retries++);
-    }
-   
-
-    return image;
-}
+            await
   await conn.sendFile(m.chat, erest, '', 'YUGI HO', m)
   global.DATABASE._data.users[m.sender].yugi = new Date * 1
-  } else m.reply('Todavia no  puedes cartear')
+  } else m.reply('Todavia no puedes cartear')
 }
 handler.help = ['carta','yugi']
 handler.tags = ['game']
@@ -37,14 +22,11 @@ handler.command = /^(carta|yugi)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
-handler.group = false
+handler.group = true
 handler.private = false
-handler.exp = 3
 
 handler.admin = false
 handler.botAdmin = false
 
 handler.fail = null
-
-module.exports = handler
 
