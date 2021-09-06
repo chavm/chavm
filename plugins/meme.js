33cf9b24407b38d3dc3292e202ca1b198df21e21
chavm <89366314+chavm@users.noreply.github.com>
2021-09-06 03:23:09 -0300

    Update meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -1,6 +1,5 @@
-async function retryResize(options, retries = 0) {
-    let { imagePath, size, quality = 60, maxRetries = 5 } = options;
 let fetch = require('node-fetch')
+let Rimage = require('../lib/image.js')
 //plugin by Samu330
 let handler  = async (m, { conn }) => {
 if (new Date - global.DATABASE._data.users[m.sender].lastme > 5400) {
@@ -9,28 +8,9 @@ let url = await fetch('https://insanebott.000webhostapp.com/meme')
 let samu = await url.json()
 let erest = samu[Math.floor(Math.random() * samu.length)]
             await conn.sendFile(m.chat, erest, '', 'MEME', m)
- let image = null;
-    try {
-        image = await Jimp.read(imagePath);
-        await image.resize(size, Jimp.AUTO);
-        await image.quality(quality);
-    } catch (e) {
-        if (retries >= maxRetries) {
-            throw e;
-        }
-                
-        image = await retryResize(options, retries++);
-    }
-   
-
-    return image;
-}
 global.DATABASE._data.users[m.sender].lastme = new Date * 1
   } else m.reply('Cargando..')
 }
-
-
-
 handler.help = ['meme','memes']
 handler.tags = ['images']
 handler.command = /^(meme|memes)$/i
